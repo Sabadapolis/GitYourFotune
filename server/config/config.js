057d025d776248105cc622820b415b5123df4202
Ryan <ryan@idea42.co>
2016-09-16 16:29:20 -0500

    Adding in Mongo Code configurations
    GitYourFortune is getting a DB, baby! WOOOOOOO

---
+++ server/config/config.js
@@ -0,0 +1,37 @@
+var _  = require('lodash');
+
+var config = {
+	dev: 'development',
+	test: 'testing',
+	prod: 'production',
+	port: process.env.PORT || 3000,
+	// 10 days in minutes
+	
+	expireTime: 24 * 60 * 10,
+	secrets: {
+		jwt: process.env.JWT || 'Charzard'
+	}
+};
+
+process.env.NODE_ENV = process.env.NODE_ENV || config.dev;
+config.env = process.env.NODE_ENV;
+
+var envConfig;
+// require could error out if
+// the file doesn't exist so lets try this statement
+// and fallback to an empty object if it does error out
+try {
+	envConfig = require('./' + config.env);
+	// just making sure the require actually
+	// got something back;
+
+	envConfig = envConfig || {};
+} catch(err) {
+	if (err) console.log(err)
+	envConfig = {};
+}
+
+// merge the two config files together
+// the envConfig file will overwirte properties
+// on the config object
+module.exports = _.merge(config, envConfig);
---
+++ server/config/production.js
@@ -0,0 +1,8 @@
+module.exports = {
+	// enabled logging for development
+	logging: false,
+	seed: false,
+	db: {
+		url: 'mongodb://localhost/GitYourFortune'
+	}
+}
