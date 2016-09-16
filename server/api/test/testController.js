427ce8974a4d56ebab6cd43ca0eff3f4369fdcb9
Ryan Ward <Ryan Ward>
2016-09-16 18:29:23 -0500

    Added balls demonstration to show how routing works

--- server/api/test/testController.js
+++ server/api/test/testController.js
@@ -1,3 +1,6 @@
 exports.get = function(req, res, next) {
 	res.json(new Date());
 }
+exports.getBalls = function(req, res, next){
+	res.json('Big Ass Balls');
+}
--- server/api/test/testRoutes.js
+++ server/api/test/testRoutes.js
@@ -1,7 +1,7 @@
 var router = require('express').Router();
 var controller = require('./testController');
 
-router.route('/')
-  .get(controller.get)
+router.route('/').get(controller.get);
+router.route('/balls').get(controller.getBalls);
 
 module.exports = router;
