1f5934dec39b1cc536e5dad7f20c5072ab1b0fe2
Chris Sabadach <Chris Sabadach>
2016-09-29 18:09:28 -0500

    Attempted to reformat answer controller in REST format

--- server/api/answer/answerController.js
+++ server/api/answer/answerController.js
@@ -4,19 +4,23 @@ var answerDto = require('./answerDto');
 //////////// GET FUNCTIONS ////////////
 
 exports.get = function (req, res) {
-    answer.find({isPublished: true}).then(function(success){
-        res.json(success);
-    }, function(error){
-        res.json(error);
+    user.findOne({ _id: req.params.id }, function (error, answerData) {
+        if (!error) {
+            res.json(answerData)
+        }
+        else {
+            res.json(error)
+        };
     })
 }
 
-exports.getAll = function (req, res){
-    answer.find().then(function(success){
+exports.getAll = function (req, res) {
+    answer.find().then(function (success) {
         res.json(success);
-    }, function (error){
+    }, function (error) {
         res.json(error);
     })
+
 }
 
 //////////// POST FUNCTIONS ////////////
@@ -26,11 +30,12 @@ exports.addAnswer = function(req, res){
     // the body into something we can use in code.
     var newAnswer = new answerDto(req.body);
 
-    answer.create(newAnswer).then(function(success){
-        res.json(success);
-    }, function(error){
-        res.json(error);
-    })
+    answer.create({ addAnswer: req.body.addAnswer }, req.body).then(function (success) {
+        res.json(success)
+    },
+        function (failure) {
+            res.json(failure)
+        });
 }
 
 //////////// TEST DATA FUNCTIONS ////////////
@@ -41,4 +46,17 @@ exports.createTest = function (req, res, next){
     }, function(error){
         res.json('fail');
     });
+}
+
+/////////// DELETE ///////////
+
+exports.removeAnswer = function (req, res) {
+    answer.remove({ removeAnswer: req.body.userName }, function (error) {
+        if (!error) {
+            res.json(error);
+        }
+        else {
+            res.json("success")
+        }
+    })
 }
