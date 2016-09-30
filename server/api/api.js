1d4608ea83abcf7eec689558e030fbe78dc64970
Chris Sabadach <Chris Sabadach>
2016-09-29 19:13:02 -0500

    forgot a character

--- server/api/api.js
+++ server/api/api.js
@@ -3,6 +3,6 @@ var router = require('express').Router();
 router.use('/test', require('./test/testRoutes'));
 router.use('/answer', require('./answer/answerRoutes'));
 router.use('/user', require('./user/userRoutes'));
-router.use('question', require('./question/questionRoutes'));
+router.use('/question', require('./question/questionRoutes'));
 
 module.exports = router;
